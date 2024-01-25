1e1f60f353428ee60c978789799eff5d25a89d48
agnesvarga1 <agikolyok@gmail.com>
2024-01-25 17:21:38 +0100

    added a method

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ createApp({
     return {
       message: "Hello World!",
       img: "https://picsum.photos/200/300",
+      likes: 55,
+      liked: false,
     };
+  },
+
+  methods: {
+    addLike: function () {
+      this.liked = !this.liked;
+      this.likes = this.likes++;
+    },
   },
 }).mount("#app");
